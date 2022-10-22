de0810d4a0390ee322a25d7574753673d0010584
студент2 <2@DESKTOP-QEFI0G7>
2022-10-22 12:20:34 +0300

    21

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System.Runtime.ExceptionServices;
 
 Console.WriteLine("Введите текст");
-string a=Console.ReadLine();
-bool b=int.TryParse(a, out int c);
+string a = Console.ReadLine();
+bool b = int.TryParse(a, out int c);
 int ferst = 0;
 int count = 0;
-int result=0;
+int result = 0;
 void Fid()
 {
-    for(int i = 1; ;)
+    for (int i = 1; ; i++)
     {
-        
+
         result += ferst + i;
         ferst = i;
-        i++;
+
         count++;
-        if(result == c)
+        if (result == c)
         {
             break;
         }
@@ -25,7 +25,7 @@ void Fid()
             Console.WriteLine();
             break;
         }
-       
+
     }
     Console.WriteLine($"{count}");
 }
